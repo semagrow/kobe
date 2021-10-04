53cc33176c8c82107421812e9c11641ef5274b96
Antonis Troumpoukis <a.troumpoukis@gmail.com>
2021-10-04 16:22:51 +0300

    delay support for virtuoso dataset template

--- examples/dataset-virtuoso/virtuoso-main/init.sh
+++ examples/dataset-virtuoso/virtuoso-main/init.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /database
 
 if [ "$DATASET_NAME" ]
-then    
+then
   if [ -f "/kobe/dataset/$DATASET_NAME/data_loaded" ]
   then
     cp /kobe/dataset/$DATASET_NAME/database/* .
@@ -12,6 +12,15 @@ then
   fi
 else
   echo "dataset name not specified"
+fi
+
+if [ "$USE_ISTIO" == "YES" ]
+then
+  until curl --head localhost:15021
+  do echo "Waiting for Sidecar"
+    sleep 3
+  done
+  echo "Sidecar available"
 fi
 
 /virtuoso-entrypoint.sh
