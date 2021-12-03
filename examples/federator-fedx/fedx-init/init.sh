b7d14b40f6712c4c8e424732ac497e73febdda85
Antonis Troumpoukis <a.troumpoukis@gmail.com>
2021-12-04 00:45:44 +0200

    delete old file first

--- examples/federator-fedx/fedx-init/init.sh
+++ examples/federator-fedx/fedx-init/init.sh
@@ -2,8 +2,6 @@
 
 FNAME="/sevod-scraper/output/"$DATASET_NAME".nt"
 
-touch $FNAME
-
-echo "<http://"$DATASET_NAME"> <http://fluidops.org/config#store> \"SPARQLEndpoint\" ." >> $FNAME
+echo "<http://"$DATASET_NAME"> <http://fluidops.org/config#store> \"SPARQLEndpoint\" ." > $FNAME
 echo "<http://"$DATASET_NAME"> <http://fluidops.org/config#SPARQLEndpoint> \""$DATASET_ENDPOINT"\" ." >> $FNAME
 echo "<http://"$DATASET_NAME"> <http://fluidops.org/config#supportsASKQueries> \"false\" ." >> $FNAME
