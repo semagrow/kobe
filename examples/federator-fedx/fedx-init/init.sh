7b18e02826911bcd9b6a5cb725ae7c758f165da6
Antonis Troumpoukis <a.troumpoukis@gmail.com>
2021-12-04 00:44:32 +0200

    disable ASK queries

--- examples/federator-fedx/fedx-init/init.sh
+++ examples/federator-fedx/fedx-init/init.sh
@@ -6,3 +6,4 @@ touch $FNAME
 
 echo "<http://"$DATASET_NAME"> <http://fluidops.org/config#store> \"SPARQLEndpoint\" ." >> $FNAME
 echo "<http://"$DATASET_NAME"> <http://fluidops.org/config#SPARQLEndpoint> \""$DATASET_ENDPOINT"\" ." >> $FNAME
+echo "<http://"$DATASET_NAME"> <http://fluidops.org/config#supportsASKQueries> \"false\" ." >> $FNAME
